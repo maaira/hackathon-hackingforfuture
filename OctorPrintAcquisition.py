6be0b8534834a5c4644b10f63ecbde4c789de835
milena.vergne <milenavergne2000@gmail.com>
2024-07-12 20:22:38 +0200

    octoprint pest api

--- OctorPrintAcquisition.py
+++ OctorPrintAcquisition.py
@@ -1,14 +1,35 @@
 from octorest import OctoRest
-def make_client(url, apikey):
-     """Creates and returns an instance of the OctoRest client.
+def make_client():
+    try:
+        client = OctoRest(url="http://octopi.local", apikey="YouShallNotPass")
+        return client
+    except Exception as e:
+        print(e)
 
-     Args:
-         url - the url to the OctoPrint server
-         apikey - the apikey from the OctoPrint server found in settings
-     """
-     try:
-         client = OctoRest(url=url, apikey=apikey)
-         return client
-     except ConnectionError as ex:
-         # Handle exception as you wish
-         print(ex)
+def get_version():
+    client = make_client()
+    message = "You are using OctoPrint v" + client.version['server'] + "\n"
+    return message
+
+def get_printer_info():
+    try:
+        client = OctoRest(url="http://octopi.local", apikey="YouShallNotPass")
+        message = ""
+        message += str(client.version) + "\n"
+        message += str(client.job_info()) + "\n"
+        printing = client.printer()['state']['flags']['printing']
+        if printing:
+            message += "Currently printing!\n"
+        else:
+            message += "Not currently printing...\n"
+        return message
+    except Exception as e:
+        print(e)
+
+def main():
+    c = make_client()
+    for k in c.files()['files']:
+        print(k['name'])
+
+if __name__ == "__main__":
+    main()
